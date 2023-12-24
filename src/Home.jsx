23287db03de1418e71bf273d16d3374addb4f015
prince <princepandey2784@gmail.com>
2023-12-24 05:40:24 +0530

    centralising card

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from './Header'
 import SearchBar from './SearchBar'
 import Card from './Card'
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
      <Header/>
      <SearchBar/>
-     <Card/>
+     <div className='container'><Card/></div>
     </div>
   )
 }
